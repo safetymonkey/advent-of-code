8d54cc968478c9286c707030307a1001e39d96d2
Jon Grover <jon.grover@gmail.com>
2021-12-01 23:21:58 -0800

    Update template, .gitignore

--- 2021/python/template.py
+++ 2021/python/template.py
@@ -10,57 +10,57 @@ Notes:
 """
 
 # Constants
-DAY = '00'
+DAY = "00"
 
 # Imports
-import functools
-import time
-
-# Helper/convenence methods
-def timer(func):
-  @functools.wraps(func)
-  def wrapper_timer(*args, **kwargs):
-    tic = time.perf_counter()
-    value = func(*args, **kwargs)
-    toc = time.perf_counter()
-    elapsed_time = toc - tic
-    print(f"Elapsed time: {elapsed_time:0.4f} seconds")
-    return value
-
-  return wrapper_timer
+from timer import timer
 
 
 def parse_input(filename):
-  """Convenience method to parse a file and return a list as input"""
-  with open(filename, 'r') as input_file:
-    # RAW
-    # input_array = [int(line) for line in input_file]
-    # return input_array
+    """Convenience method to parse a file and return a list as input"""
+    with open(filename, "r") as input_file:
+        # RAW
+        # input_array = [int(line) for line in input_file]
+        # return input_array
 
-    # REGEX (requires import)
-    # for line in input_file:
-    #   match = re.match(r'\[(.*)\] (.*)', line)
-    # return X
+        # REGEX (requires import)
+        # for line in input_file:
+        #   match = re.match(r'\[(.*)\] (.*)', line)
+        # return X
+        return
+
+
+@timer
+def print_output(part: int, input: list):
+    """Convenience method to print output to console"""
+    if part == 1:
+        output = part1(input)
+        print(f"Part 1: {output}")
+    elif part == 2:
+        output = part2(input)
+        print(f"Part 2: {output}")
 
 
 @timer
 def part1(input):
-  return
+    output = None
+    return output
 
 
 @timer
 def part2(input):
-  return
+    output = None
+    return output
 
 
 if __name__ == "__main__":
-  INPUT = f"inputs/day{DAY}.txt"
-  TEST_INPUT = f"inputs/day{DAY}_test.txt"
+    INPUT = f"inputs/day{DAY}.txt"
+    TEST_INPUT = f"inputs/day{DAY}_test.txt"
 
-  assert part1(parse_input(TEST_INPUT)) == 514579
-  print(f"Part 1: {part1(parse_input(INPUT))}")
+    assert part1(parse_input(TEST_INPUT)) == 150
+    print_output(1, parse_input(INPUT))
 
-  # UNCOMMENT AFTER PART 1
-  # print('\n')
-  # assert part2(parse_input(TEST_INPUT)) == 241861950
-  # print(f"Part 2: {part2(parse_input(INPUT))}")
+    # UNCOMMENT AFTER PART 1
+    # print('\n')
+    # assert part2(parse_input(TEST_INPUT)) == 900
+    # print_output(2, parse_input(INPUT))
